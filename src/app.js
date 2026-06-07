ae374a27ab5a7481c5067d06c25f0009b72c29bf
MayurHajare-code <147743627+MayurHajare-code@users.noreply.github.com>
2026-06-07 21:26:54 +0530

    add deployed frontend link in app.js

--- src/app.js
+++ src/app.js
@@ -12,7 +12,8 @@ app.use(express.json());
 app.use(
     cors({
         origin: [
-            "http://localhost:5173"
+            "http://localhost:5173",
+            "https://brewmaster-frontend-333.vercel.app/"
         ],
         credentials: true,
     })
